41263c6b9cf037f80fa69b8f0e14bd84552fa4e9
amalnath369 <amalnaths640@gmail.com>
2025-05-26 15:31:20 +0530

    Initial commit

--- ondcsubscribe/urls.py
+++ ondcsubscribe/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from ondc.views import ondc_site_verification
+# from ondc.views import ondc_site_verification
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ondc.urls')),
