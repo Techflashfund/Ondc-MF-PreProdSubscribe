d60a96f38fbf8627e4b129632db008fbc44e20e5
amalnath369 <amalnaths640@gmail.com>
2025-05-26 11:25:31 +0530

    Initial commit

--- ondcsubscribe/settings.py
+++ ondcsubscribe/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-hbr5rk4c9&&y#7#h55g2m0f*o)(ho#u8a#b9jd-w%u)ffry_90
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
